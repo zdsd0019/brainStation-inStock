c4cea96f2841631cd7da0f000611a44f0e17afe7
brandonwong22 <brandonwong2608@gmail.com>
2020-06-02 17:42:39 -0400

    created tablet view of inventory page

--- client/src/components/InventoryItem/InventoryItem.js
+++ client/src/components/InventoryItem/InventoryItem.js
@@ -24,43 +24,66 @@ const InventoryItem = (props) => {
     status = "Out of Stock";
   }
 
+  let headerClassName = "inventory__header3";
   let descriptionContainerClassName = "inventory__description-container";
+  let descriptionClassName = "inventory__paragraph";
+  let removeButtonClassName = "inventory__remove";
+
   if (first === "first") {
+    headerClassName += " inventory__header3--first";
     descriptionContainerClassName += " inventory__description-container--first";
+    descriptionClassName += " inventory__paragraph--first";
+    removeButtonClassName += " inventory__remove--first";
   }
 
   return (
     <div className="inventory__card">
-      <div className={descriptionContainerClassName}>
+      <div
+        className={
+          descriptionContainerClassName +
+          " inventory__description-container--name"
+        }
+      >
         <div className="inventory__header3-container">
-          <h3 className="inventory__header3">ITEM</h3>
+          <h3 className={headerClassName}>ITEM</h3>
           <img
             src={remove}
             className="inventory__remove inventory__remove--mobile"
           />
         </div>
-        <p className="inventory__product-name">{name}</p>
-        <p className="inventory__paragraph">{description}</p>
+        <div>
+          <p className="inventory__product-name">{name}</p>
+          <p className="inventory__paragraph inventory__paragraph--description">
+            {description}
+          </p>
+        </div>
       </div>
 
       <div className={descriptionContainerClassName}>
-        <h3 className="inventory__header3">LAST ORDERED</h3>
-        <p className="inventory__paragraph">{lastOrdered}</p>
+        <h3 className={headerClassName}>LAST ORDERED</h3>
+        <p className={descriptionClassName}>{lastOrdered}</p>
       </div>
 
       <div className={descriptionContainerClassName}>
-        <h3 className="inventory__header3">LOCATION</h3>
-        <p className="inventory__paragraph">{city}</p>
+        <h3 className={headerClassName}>LOCATION</h3>
+        <p className={descriptionClassName}>{city}</p>
       </div>
 
       <div className={descriptionContainerClassName}>
-        <h3 className="inventory__header3">QUANTITY</h3>
-        <p className="inventory__paragraph">{quantity}</p>
+        <h3 className={headerClassName}>QUANTITY</h3>
+        <p className={descriptionClassName}>{quantity}</p>
       </div>
 
       <div className={descriptionContainerClassName}>
-        <h3 className="inventory__header3">STATUS</h3>
-        <p className="inventory__paragraph">{status}</p>
+        <h3 className={headerClassName}>STATUS</h3>
+        <p className={descriptionClassName}>{status}</p>
+      </div>
+
+      <div className="inventory__tablet-button-container">
+        <img
+          src={remove}
+          className={removeButtonClassName + " inventory__remove--tablet"}
+        />
       </div>
     </div>
   );
