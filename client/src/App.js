65eb68df037c65da539776cb79e5b67567be127d
kumail <zdsd0019@gmail.com>
2020-06-02 00:56:50 -0400

    Created header and fixed file structure for client side

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
 // import warehouses from '';
 // import inventory from '';
+import Header from './components/header/header';
 
 
 class App extends Component {
   render() {
     return (
      <>
-     {/* <Router>
+      <Router>
+     <Header />
       <Switch>
-        <Route path='/' exact component={ warehouses } />; {/* redirects to warehouse */}
+        {/* <Route path='/' exact component={ warehouses } />; {/* redirects to warehouse */}
         {/* <Route path="/warehouses" component={ warehouses } /> {/* All warehouse List */}
         {/* <Route path="/inventory" component = { inventory } /> {/* All Inventory List */}
         {/* <Route path='/warehouses/:id' component={ warehouses } />; {/* Warehouse details */}
         {/* <Route path='/inventory/:id' component={ inventory } />; {/* Inventory details */}
-      {/* </Switch>
-      </Router> */}
+       </Switch>
+      </Router> 
      </>
     );
   }
--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,1 +1,23 @@
-ignore
+import React from 'react';
+import './header.scss';
+import Logo from '../logo/logo';
+import { Link } from 'react-router-dom'
+
+
+function Header() {
+	return (
+		<span className='header-container'>
+            <span className='header'>
+				<Link className='header-logo' to="/">
+					<Logo />
+				</Link>
+            </span>
+            <nav className='btn' >
+            <input type='button' value='Inventory' className='btn-inventory btn-active' />
+            <input type='button' value='Locations' className='btn-locations' />
+            </nav>
+		</span>
+	);
+}
+
+export default Header;
