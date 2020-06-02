373837e2750811f08ab08b2d41ced12e2a2a9f13
brandonwong22 <brandonwong2608@gmail.com>
2020-06-02 13:06:54 -0400

    created component and css for inventory page

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
-import WarehouseList from './components/WarehouseList/WarehouseList';
-// import inventory from '';
-
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import WarehouseList from "./components/WarehouseList/WarehouseList";
+import Inventory from "./pages/Inventory/Inventory";
 
 class App extends Component {
   render() {
     return (
-     <>
-     <Router>
-      <Switch>
-        <Route path='/' exact component={ WarehouseList } /> {/* redirects to warehouse */}
-        <Route path="/warehouses" component={ WarehouseList } /> {/* All warehouse List
+      <>
+        <Router>
+          <Switch>
+            <Route path="/" exact component={Inventory} />{" "}
+            {/* redirects to warehouse */}
+            <Route path="/warehouses" component={WarehouseList} />{" "}
+            {/* All warehouse List
         {/* <Route path="/inventory" component = { inventory } /> {/* All Inventory List */}
-        {/* <Route path='/warehouses/:id' component={ warehouses } />; {/* Warehouse details */}
-        {/* <Route path='/inventory/:id' component={ inventory } />; {/* Inventory details */}
-      </Switch>
-      </Router>
-     </>
+            {/* <Route path='/warehouses/:id' component={ warehouses } />; {/* Warehouse details */}
+            {/* <Route path='/inventory/:id' component={ inventory } />; {/* Inventory details */}
+          </Switch>
+        </Router>
+      </>
     );
   }
 }
---
+++ client/src/pages/Inventory/Inventory.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./Inventory.scss";
+import axios from "axios";
+
+class Inventory extends Component {
+  render() {
+    return <div className="inventory">Inventory</div>;
+  }
+}
+
+export default Inventory;
