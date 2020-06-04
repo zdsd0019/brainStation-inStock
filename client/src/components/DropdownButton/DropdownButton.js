e145fb2e50737c8b6bc63fbb2e2c49eb448eefe8
Paulo <paulovlutz@gmail.com>
2020-06-04 13:26:29 -0400

    add dropdown button and styles for all breakpoints

---
+++ client/src/components/DropdownButton/DropdownButton.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Dropdown from 'rc-dropdown';
+import Menu, { Item as MenuItem } from 'rc-menu';
+import 'rc-dropdown/assets/index.css';
+import './DropdownButton.scss';
+import remove from "../../assets/Icons/SVG/Icon-kebab-default.svg";
+
+class DropdownButton extends Component {
+    removeButton = (
+        <Menu className="inventory__dropdown-menu">
+            <MenuItem className="inventory__dropdown-menuItem" option="remove">Remove</MenuItem>
+        </Menu>
+    );
+
+    render() {
+        return (
+            <div className="inventory__dropdown">
+                <Dropdown overlay={this.removeButton} trigger={["click"]} animation="slide-up" placement="bottomRight">
+                    <a href="" className="inventory__dropdown-removeButton">
+                        <img
+                            src={remove}
+                            className={this.props.removeButtonClassName}
+                            alt='click here to remove item'
+                        />
+                    </a>
+                </Dropdown>
+            </div>
+        )  
+    }
+}
+
+export default DropdownButton;
